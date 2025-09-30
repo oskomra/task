eef0453d00e0f2c22648340e60653a99217c7155
oskomra <oskarskomra5@gmail.com>
2025-09-30 19:20:52 +0200

    Initial commit

---
+++ InvoiceServiceBackend/src/main/java/osk/sko/InvoiceServiceBackend/InvoiceServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package osk.sko.InvoiceServiceBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceServiceBackendApplication.class, args);
+	}
+
+}
---
+++ InvoiceServiceBackend/src/test/java/osk/sko/InvoiceServiceBackend/InvoiceServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package osk.sko.InvoiceServiceBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceServiceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
